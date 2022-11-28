eaf65a871831d3d14f9caa11955faddada5d02f2
Dillon Hughes <dillonhughes29@gmail.com>
2022-11-28 15:27:57 -0500

    first commit

---
+++ CSharpNotes/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Data Types
+// What is a statically typed language?
+// You have to specify the type of variable or function you want to create
+// Javascript
+// var name = "Dillon";
+//C#
+string name = "Dillon";
+int number = 7;
+double dec = 3.14;
+float floatvalue = 1.2f;
+bool isTired = true;
+
+// Lists and Dictionaries
+// Array and List
+// Arrays are fixed length
+string[] groceryList = new string[4];
+// [null, null, null, null]
+string[] groceryList2 = {"Carrots", "Turkey", "Bread", "Milk"};
+
+groceryList[2] = "Ham";
+// null, null, Ham, null
+Console.WriteLine(groceryList[2]);
+
+//Size of an Array
+Console.WriteLine(groceryList.Length);
+
+// Lists are variable lenght 
+List<int> numberList = new List<int>();
+numberList.Add(3);
+numberList.Add(3);
+numberList.Add(4);
+// Remove at the location index 3
+// numberList.RemoveAt(3);
+// First parameter is the index, second is the value
+numberList.Insert(1,5);
+// This removes the value of 3
+numberList.Remove(3);
+//Size of a List
+Console.WriteLine(numberList.Count);
+
+foreach (int i in numberList)  {
+Console.WriteLine(i);
+}
+
+
---
+++ Fundamentals_1/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] numbersArr = {0,1,2,3,4,5,6,7,8,9};
+foreach (int i in numbersArr){
+Console.WriteLine(i);
+}
+
+string[] namesArr = {"Tim", "Martin", "Nikki", "Sarah"};
+foreach (string i in namesArr){
+    Console.WriteLine(i);
+}
+
+int[] statements = new int[10];
+for (int i = 0; i < statements.Length; i++){
+    if(i % 2 == 0){
+        Console.WriteLine (true);
+    }
+    else {
+        Console.WriteLine (false);
+    }
+}
+
+List<string> flavorList = new List<string>(5);
+flavorList.Add("Vanilla");
+flavorList.Add("Chocolate");
+flavorList.Add("Rocky Road");
+flavorList.Add("Mint");
+flavorList.Add("Strawberry");
+// flavorList.RemoveAt(2);
+Console.WriteLine(flavorList[2]);
+Console.WriteLine(flavorList.Count);
+foreach (string i in flavorList){
+    Console.WriteLine(i);
+}
+
+Dictionary<string,string> preference = new Dictionary<string, string>();
+preference.Add("Tim", "Mint");
+
+preference.Add("Martin", "Vanilla");
+
+preference.Add("Nikki", "Chocolate");
+
+preference.Add("Sarah", "Strawberry");
+
+foreach (KeyValuePair<string,string> entry in preference){
+    Console.WriteLine($"{entry.Key} - {entry.Value}");
+}
