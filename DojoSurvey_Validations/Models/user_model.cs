4d3032e71a9a38da1996ccd390752c16e1536d4f
Dillon Hughes <dillonhughes29@gmail.com>
2022-12-04 14:10:41 -0500

    updates

---
+++ DojoSurvey_Validations/Models/user_model.cs
@@ -0,0 +1,12 @@
+#pragma warning disable CS8618
+namespace DojoSurvey_Validations.Models;
+    public class User
+{
+    public string Name {get;set;}
+
+    public string DojoLocation {get;set;}
+
+    public string FavLanguage {get;set;}
+
+    public string Comment {get;set;}
+}
